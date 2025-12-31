d0c39bcea09d24b3c836a2aa44232772cf67782a
Tanvir Rayim <tanvirrayim11@gmail.com>
2025-12-31 15:29:34 +0600

    Create BMI Calculator

---
+++ BMI Calculator/bmi.py
@@ -0,0 +1,6 @@
+height = input("Enter your height in meters: ")
+weight = input("Enter your weight in kilograms: ")
+
+bmi = float(weight) / (float(height) ** 2)
+
+print("Your BMI is:", bmi)
---
+++ Where this Leads to/path.py
@@ -0,0 +1,2 @@
+print("Hello! Welcome to 'Where This Leads To'!\nBased on your choices, we will see which country you end up in.")
+choice1 = input("Do you prefer ice or ")
