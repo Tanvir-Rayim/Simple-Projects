d7a6fa36bffa30d1100af4984ee4c93009d990c7
Tanvir Rayim <tanvirrayim11@gmail.com>
2026-01-01 07:57:30 +0600

    Shows your condition based on BMI

--- BMI Calculator/bmi.py
+++ BMI Calculator/bmi.py
@@ -3,4 +3,11 @@ weight = input("Enter your weight in kilograms: ")
 
 bmi = float(weight) / (float(height) ** 2)
 
-print("Your BMI is:", bmi)
+if bmi < 18.5:
+    print(f"Your BMI is {bmi:.2f}, you are underweight.")
+elif 18.5 <= bmi < 25:
+    print(f"Your BMI is {bmi:.2f}, you have a normal weight.")
+elif 25 <= bmi < 30:
+    print(f"Your BMI is {bmi:.2f}, you are overweight.")
+else:
+    print(f"Your BMI is {bmi:.2f}, you are obese.")
