b31cacd5766084eaf189560ae1dbe66feed8fe04
Tanvir Rayim <tanvirrayim11@gmail.com>
2026-01-01 08:02:00 +0600

    Check Elgility and Calculate Ticket

---
+++ Ticket Calculate/ticket.py
@@ -0,0 +1,17 @@
+print("Welcome to the rollercoaster!")
+height = int(input("What is your height in cm? "))
+age = int(input("What is your age? "))
+photo = input("Do you want to take a photo? (y/n) ")
+
+if height >= 120:
+    if age < 12:
+        ticket = 5
+    elif 12 <= age <= 18:
+        ticket = 7
+    else:
+        ticket = 12
+    if photo == "y":
+        ticket += 3
+    print(f"You can ride the rollercoaster! The ticket is ${ticket}")
+else:
+    print("You can not ride the rollercoaster!")
