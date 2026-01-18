b2e51b295534bb36825cb18938aa4107d729be96
Tanvir Rayim <tanvirrayim11@gmail.com>
2026-01-18 06:20:03 +0600

    Fixed Errors

--- Rock Paper Scissors/rock_paper_scissors.py
+++ Rock Paper Scissors/rock_paper_scissors.py
@@ -29,39 +29,31 @@ scissors = '''
 
 values = [rock, paper, scissors]
 user = int(input("What do you choose? 0: Rock, 1: Paper, 2: Scissors: "))
-if user == 0:
+pick = random.choice(values)
+if 0 <= user < 3:
     user_pick = values[user]
     print("You picked ", user_pick)
-    pick = random.choice(values)
     print("computer picked ", pick)
-elif user == 1:
-    user_pick = values[user]
-    print("You picked ", user_pick)
-    pick = random.choice(values)
-    print("computer picked ", pick)
+    if user_pick == rock:
+        if pick == paper:
+            print("You Loose!")
+        elif pick == scissors:
+            print("You Win!")
+        else:
+            print("Draw!")
+    elif user_pick == paper:
+        if pick == scissors:
+            print("You Loose!")
+        elif pick == rock:
+            print("You Win!")
+        else:
+            print("Draw!")
+    else:
+        if pick == rock:
+            print("You Loose!")
+        elif pick == paper:
+            print("You Win!")
+        else:
+            print("Draw!")
 else:
-    user_pick = values[user]
-    print("You picked ", user_pick)
-    pick = random.choice(values)
-    print("computer picked ", pick)
-if user_pick == rock:
-    if pick == paper:
-        print("You Loose!")
-    elif pick == scissors:
-        print("You Win!")
-    else:
-        print("Draw!")
-elif user_pick == paper:
-    if pick == scissors:
-        print("You Loose!")
-    elif pick == rock:
-        print("You Win!")
-    else:
-        print("Draw!")
-else:
-    if pick == rock:
-        print("You Loose!")
-    elif pick == paper:
-        print("You Win!")
-    else:
-        print("Draw!")
+    print("Enter a valid number!")
